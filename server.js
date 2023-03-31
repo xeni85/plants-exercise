75233af4e0fbdf6831df741b78f01be089d564c6
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-03-31 16:14:48 -0400

    started working on it

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
 //-----Requests
-
 const express = require('express');
 const app = express();
-
-const fruits = [
-    {
-        name:'apple',
-        color: 'red',
-        readyToEat: true
-    },
-    {
-        name:'pear',
-        color: 'green',
-        readyToEat: false
-    },
-    {
-        name:'banana',
-        color: 'yellow',
-        readyToEat: true
-    }
-];
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 
+const fruits = require('./models/fruits');
 //----index
-app.get('/', (req, res) => {
-    res.send(<h1>Root/ Index Page</h1>);
-});
 
 
-app.get(':indexOfPlantsArray', (req, res) => {
-    res.send(fruits[req.params.indexOfPlantsArray]);
+app.get('/fruits/', (req, res) => {
+    res.send(fruits);
 });
 
 app.get('/fruits/:indexOfFruitsArray', (req, res) => {
-    res.send(fruits[req.params.indexOfFruitsArray]);
+    res.render('Show');
 });
 
 app.listen(3000, () => {
     console.log('listening');
 });
+
---
+++ views/Show.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Show extends Component {
+  render() {
+    return (
+      <h1>Show Page</h1>
+    )
+  }
+}
+
+module.exports = Show;
