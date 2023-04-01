5d34321c0e090c2b637dd8deb88721a836466ed2
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-01 12:30:58 -0400

    working on functionality

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.engine('jsx', require('express-react-views').createEngine());
 const fruits = require('./models/fruits');
 //----index
 
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+app.use(express.urlencoded({extended:false}));
 
 app.get('/fruits/', (req, res) => {
     res.render('Index', {fruits: fruits});
@@ -17,6 +23,14 @@ app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     res.render('Show', {
         fruit: fruits[req.params.indexOfFruitsArray]
     });
+});
+
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+
+app.post('/fruits', (req, res) => {
+    res.send('hi');
 });
 
 app.listen(3000, () => {
